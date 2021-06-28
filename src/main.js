bf79df2abeec3a9e85c61ca98be47b6b77ac8d67
santosh <santosh.ece06@gmail.com>
2021-06-28 11:30:32 +0530

    day 5

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// WE ARE NOT DONE HERE.
+import { sum, subtract } from "./calculator.js";
+
+let output = sum(10, 20);
+console.log("SUM", output);
+
+let output1 = subtract(20, 10);
+console.log("DIFF", output1);
