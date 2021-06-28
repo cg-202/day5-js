ce8ac9829c93b49d21ce28e23c94f01ae020b23d
santosh <santosh.ece06@gmail.com>
2021-06-28 11:48:32 +0530

    import and export.

---
+++ src/v1.main.js
@@ -0,0 +1,9 @@
+// WE ARE NOT DONE HERE.
+import { sum, subtract } from "./v1.calculator.js";
+import { multiply } from "./calculator.js";
+
+let output = sum(10, 20);
+console.log("SUM", output);
+
+let output1 = subtract(20, 10);
+console.log("DIFF", output1);
