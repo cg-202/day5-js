b9aba237aa5611b00d384f1b0c47e5366350943c
santosh <santosh.ece06@gmail.com>
2021-06-30 10:12:55 +0530

    class,

---
+++ src/user.js
@@ -0,0 +1,8 @@
+class User {
+  // speical function :: and it has a  name :: constructor
+  constructor(username, email, mobile) {
+    this.username = username;
+    this.email = email;
+    this.mobile = mobile;
+  }
+}
